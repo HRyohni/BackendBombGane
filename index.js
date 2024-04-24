53b89bdf2f938a47c3b63c5918c54dfeae5e90fc
Yohni <62948056+HRyohni@users.noreply.github.com>
2024-04-24 20:52:59 +0200

    commit

--- index.js
+++ index.js
@@ -187,7 +187,7 @@ app.get('/api/gamemode/fetch-gamemodes', async (req, res) => {
 app.get('/api/gameMode/:gameModeId', async (req, res) => {
     try {
         const name = req.params;
-        const gameMode = await GameMode.findOne({name: "colors"});
+        const gameMode = await GameMode.findOne({name: name});
         res.status(200).send(gameMode);
     } catch (error) {
         console.log(error);
